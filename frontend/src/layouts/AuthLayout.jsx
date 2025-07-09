21a44e4ece22ba7a8b813a2cc0fbbc35e10d6e01
OliverHugo09 <oliverlucero.dev@gmail.com>
2025-07-09 13:40:55 -0500

    login page done

--- frontend/src/layouts/AuthLayout.jsx
+++ frontend/src/layouts/AuthLayout.jsx
@@ -2,10 +2,8 @@ import { Outlet } from "react-router-dom";
 
 export default function AuthLayout() {
   return (
-    <div className="min-h-screen w-full flex items-center justify-center bg-gray-100">
-      <div className="w-full max-w-md md:max-w-lg lg:max-w-xl px-4 sm:px-6">
+    <div className="w-full flex items-center justify-center bg-gray-100">
         <Outlet />
-      </div>
     </div>
   );
 }
