8fe18faff18a5f98cb0a9d9d18d6d2a0346ea871
OliverHugo09 <oliverlucero.dev@gmail.com>
2025-07-11 12:03:34 -0500

    verify, register pages

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -2,9 +2,6 @@ import {
   Input,
   Button,
   Typography,
-  Card,
-  CardBody,
-  CardFooter,
 } from "@material-tailwind/react";
 
 import { useState } from "react";
@@ -26,16 +23,80 @@ export default function Register() {
     e.preventDefault();
     console.log(form);
 
-    // Redirección condicional (puedes reemplazar con lógica real)
-    if (form.Email === "admin@example.com") {
-      console.log("Redirigir a 2FA...");
-    } else {
-      alert("Correo no autorizado para 2FA.");
-    }
+
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+    <>
+    <div className="bg-register">
+        
+    </div>
+    <div className="absolute register-form flex flex-col p-4 rounded-2xl border-white border-8 items-center justify-center w-[20em] sm:w-[20em] md:w-[25em] lg:w-[25em] xl:w-[25em]">
+      <Typography variant="h4" className="mb-6 text-center text-gray-800">
+        Registrate
+      </Typography>
+
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+            <Input
+              label="Nombre"
+              name="Name"
+              value={form.Name}
+              onChange={handleChange}
+              required
+            />
+            <Input
+              label="Apellido"
+              name="Lastname"
+              value={form.Lastname}
+              onChange={handleChange}
+              required
+            />
+            <Input
+              label="Usuario"
+              name="Username"
+              value={form.Username}
+              onChange={handleChange}
+              required
+            />
+            <Input
+              label="Correo Electrónico"
+              name="Email"
+              type="email"
+              value={form.Email}
+              onChange={handleChange}
+              required
+            />
+            <Input
+              label="Contraseña"
+              name="Password"
+              type="password"
+              value={form.Password}
+              onChange={handleChange}
+              required
+            />
+
+        <Button type="submit" fullWidth>
+          Registrarse
+        </Button>
+      </form>
+    </div>
+    </>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <Card className="w-full max-w-md p-6">
         <form onSubmit={handleSubmit}>
           <CardBody className="flex flex-col gap-4">
@@ -89,6 +150,6 @@ export default function Register() {
           </CardFooter>
         </form>
       </Card>
-    </div>
+    </div> */
   );
 }
